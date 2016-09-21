1474afcf2692095e66e0feb70b9f57768fb94c2a
abhaynarayan09 <abhaynarayan09@gmail.com>
2016-09-22 00:56:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/abhay1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'abhay1.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
